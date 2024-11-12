47ec6ab6190894366e986e328265d1cfa50a1fba
Deathkonn <100820134+Deathkonn@users.noreply.github.com>
2024-11-12 10:35:44 +1000

    getting better, no content

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar'
 import Home from './components/Home'
 import Projects from './pages/Projects'
 import Topbar from './components/Topbar';
+import About from './pages/About';
 import './styles/App.css'
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const handleToggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <Router>
-      <div className="App">
-        <Topbar />
-        <Sidebar />
-        <div classname="content">
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/projects" element={<Projects />} />
-          </Routes>
+      <div className="app">
+        <Topbar onToggleSidebar={handleToggleSidebar} />
+        <div className={`app-content ${isSidebarOpen ? 'sidebar-open' : 'sidebar-closed'}`}>
+          <Sidebar isOpen={isSidebarOpen} onToggleSidebar={handleToggleSidebar} />
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </Router>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "../styles/Sidebar.css"
+import '../styles/Sidebar.css';
+import HamburgerIcon from '../assets/hamburger-icon.svg';
 
-const Sidebar = () => {
+const Sidebar = ({ isOpen, onToggleSidebar }) => {
   return (
-    <div className="sidebar">
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/about">About Me</Link></li>
-        </ul>
-      </nav>
+    <div className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
+      <button className="toggle-button" onClick={onToggleSidebar}>
+        <img
+          src={HamburgerIcon}
+          alt="Toggle Menu"
+          className="hamburger-icon"
+        />
+      </button>
+      {isOpen && (
+        <div>
+          <h2>Pages</h2>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/projects">Projects</Link></li>
+              <li><Link to="/about">About Me</Link></li>
+            </ul>
+          </nav>
+        </div>
+      )}
     </div>
   );
 };
